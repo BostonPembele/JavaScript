03fbc4d8604bcaaf1c03ac34c41848bfff571015
Boston <g00436544@atu.ie>
2026-02-07 13:42:41 +0000

    final changes to this folder possibly

--- Basics/index.js
+++ Basics/index.js
@@ -1,2 +1,14 @@
 console.log('Hello, World!');
-alert("hello world")
+console.log("i like pizza");
+
+//window.alert("This is an alert");
+//window.alert("i like pizza");
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza";
+
+//This is a comment
+
+/*
+this is a comment
+*/
