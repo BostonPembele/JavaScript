51fbb3186d1eac27abaa364e9db95f4f1eaf5197
Boston <g00436544@atu.ie>
2026-02-07 15:01:04 +0000

    Caught up on variables

--- Variables/index.js
+++ Variables/index.js
@@ -1,17 +1,42 @@
 document.getElementById("myH1").innerHTML = "Welcome to My Website";
 document.getElementById("myP").textContent = "This is a sample text from JS";
 
-let age = 25;
-let price = 10.99;
-let gpa = 3.5;
+//Variables
+//let age = 25;
+//let price = 10.99;
+//let gpa = 3.5;
 
-let firstName = "Boston";
-
-console.log(typeof firstName);
-console.log(firstName);
-console.log("Your name is "+firstName);
-
+//Number data type
 //console.log(typeof price);
 //console.log("You are "+age +"years old.");
 //console.log("The price of the item is $" +price);
 //console.log("Your GPA is " +gpa);
+
+//let firstName = "Boston";
+//let favouriteFood = "Pizza";
+//let email = "g00436544@atu.ie";
+
+//String data type
+//console.log(typeof firstName);
+//console.log(firstName);
+//console.log("Your name is "+firstName);
+//console.log("My favourite food is "+favouriteFood);
+//console.log("Please contact me at: "+email);
+
+//let online = true;
+//let forSale = false
+//let isStudent = true;
+
+//Boolean data type
+//console.log(typeof online);
+//console.log("Boston is online: " +online);
+//console.log("Is this car for sale? " +forSale);
+//console.log("Enrollled: " +isStudent);
+
+let fullName = "Boston Pembele";
+let age = 20;
+let isStudent = true;
+
+document.getElementById("p1").textContent = "My name is "+fullName;
+document.getElementById("p2").textContent = "My age is " +age;
+document.getElementById("p3").textContent = "Student: " +isStudent;
