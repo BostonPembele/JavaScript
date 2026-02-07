03423e8fd818fc7ce91b4f11e700cb4a305c5907
Boston <g00436544@atu.ie>
2026-02-07 14:31:40 +0000

    Moved on to variables halfway there

---
+++ Variables/index.js
@@ -0,0 +1,17 @@
+document.getElementById("myH1").innerHTML = "Welcome to My Website";
+document.getElementById("myP").textContent = "This is a sample text from JS";
+
+let age = 25;
+let price = 10.99;
+let gpa = 3.5;
+
+let firstName = "Boston";
+
+console.log(typeof firstName);
+console.log(firstName);
+console.log("Your name is "+firstName);
+
+//console.log(typeof price);
+//console.log("You are "+age +"years old.");
+//console.log("The price of the item is $" +price);
+//console.log("Your GPA is " +gpa);
