d0e202897ba287045027cdb8d54739102ab3f0b3
Boston <g00436544@atu.ie>
2026-02-07 15:47:37 +0000

    Started with arithmetic operators

---
+++ Arithmetic operators/index.js
@@ -0,0 +1,17 @@
+let students = 30;
+
+//Addition:students = students + 1;
+//Subtraction: students = students - 1;
+//Multiplication: students = students * 2;
+//Division: students = students / 2;
+//Exponent/ Multiply to the power: students = students ** 2;#
+//Modulus/ Remainder: students = students % 7;
+
+//students += 1; //students = students + 1;
+//students -=1; //students = students - 1;
+//students *= 2; //students = students * 2;
+//students /= 2; //students = students / 2;
+//students **= 2; //students = students ** 2;
+//students %= 21; //students = students % 21;
+
+console.log(students);
